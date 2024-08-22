b16217ee7202df1be20f72fd326fcff2e2188a36
Garcia0990 <juan.garciaduque09@gmail.com>
2024-08-22 07:47:12 -0500

    Parcial 1_ Juan.java

--- app/src/main/java/parcial1g1/App.java
+++ app/src/main/java/parcial1g1/App.java
@@ -1,11 +1,51 @@
+package parcial;
+import java.util.Scanner;
 
-package parcial1g1;
 
 public class App {
+   
 
     public static void main(String[] args) {
-        
-        // Implementar la logica de solucion
+        try {   
+            // Implementar la logica de solucion
+            int numVehiculos = 0;
+            float distanciaMetros = 0;
+            float consumoTotal = 0; //almacenar el consumo total
+            float consumoVehiculo = 0;
+            float kmRecorrido = 0;
+                
+
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Cuantos vehiculos participaron"); // Preguntar al usuario cuántos vehículos participaron
+            numVehiculos = sc.nextInt();
+             
+            for (int i = 1; i <= numVehiculos; i++){ // Iterar los vehículos
+                System.out.print("Ingrese la distancia recorrida por el vehículo " + i + " en metros: ");
+                distanciaMetros = sc.nextFloat();
+
+                while (distanciaMetros <= 0) { // Validar que la Distancia sea positiva
+                    System.out.print("La distancia debe ser un número positivo. Ingrese nuevamente: ");
+                    distanciaMetros = sc.nextFloat();    
+                }            
+                //convertir la distancia de metros a kilómetros 
+                kmRecorrido = distanciaMetros/1000;
+                System.out.println("vehiculo: " + i + " " + (kmRecorrido) + " km");
+                
+
+                // Calcular el consumo de combustible para cada vehículo
+                consumoVehiculo = calcularConsumoGasolina(kmRecorrido);
+                System.out.println(consumoVehiculo + "Litros de gasolina consumidos: ");
+                
+
+                consumoTotal += consumoVehiculo;
+            }
+
+            System.out.println("Consumo total de combustible por los 3 vehículos: " + consumoTotal + " litros");
+        }
+            
+        catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
         
     }
 
@@ -17,8 +57,18 @@ public class App {
     */
     //------------------------------------------------------------------
     //Implemente la funcion en este espacio
+    public static float calcularConsumoGasolina(float kmRecorrido){
+        try {
+            
+            return ((kmRecorrido * 55) / 750); 
+                            
+        } catch (Exception e) {
+            return -1;
+        }        
+    }
+    //------------------------------------------------------------------
+}
     
 
-    //------------------------------------------------------------------
 
-}
+
